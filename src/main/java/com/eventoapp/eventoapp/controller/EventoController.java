0170ad8b890cf771d537df9cce24254d96e257f8
mariorafael21 <mario.rafael99@outlook.com>
2021-07-03 15:22:52 -0300

    Initial commit

---
+++ src/main/java/com/eventoapp/eventoapp/controller/EventoController.java
@@ -0,0 +1,28 @@
+package com.eventoapp.eventoapp.controller;
+
+import com.eventoapp.eventoapp.models.Evento;
+import com.eventoapp.eventoapp.repository.EventoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class EventoController {
+    @Autowired
+    private EventoRepository er;
+
+    @RequestMapping(value = "/cadastrarEvento", method = RequestMethod.GET)
+    public String form(){
+
+        return "evento/formEvento";
+    }
+
+    @RequestMapping(value = "/cadastrarEvento", method = RequestMethod.POST)
+    public String form(Evento evento){
+        er.save(evento);
+        return "redirect: evento/formEvento";
+    }
+
+}
+
